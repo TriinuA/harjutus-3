be221437774c9731b90fbc1b2c38852e0668d919
TriinuA <triinu.a@gmail.com>
2014-09-09 15:52:20 +0300

    Inntial commit
    Inntial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>harjutus-3 - Andmetüübid</title>
+</head>
+  <body>
+  	 <h2>Muutujad</h2>
+	  	 <?php 
+	  	   $randomnumber = 10;
+	  	   echo $randomnumber;
+	  	   echo "<br>"; 
+	  	 ?>
+	  	 <?php  
+	  	   $randomnumber = 22;
+	  	   echo $randomnumber;
+	  	 ?>
+	 <h3>Stringid</h3> 	 
+	   <?php 
+	     $message = "Hello World!<br>";
+	     echo $message;
+	     $message2 = 'Hello :)';
+		 echo $message2;
+
+
+	   ?>
+
+
+
+  </body>
+</html>	
